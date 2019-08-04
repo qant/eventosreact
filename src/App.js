9b50bda253e54891569c17d5b17882eaa65eb628
qant <anseme@g,ail.com>
2019-08-04 19:15:26 +0200

    ContextProvider token and axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import Header from "./components/Header";
+import CategoryProvider from "./context/CategoryContext";
 
 //https://www.eventbriteapi.com/v3/users/me/?token=GYEGZ33PYVDUFK7DWAD6
 
 function App() {
   return (
-    <Fragment>
+    <CategoryProvider>
       <Header />
-    </Fragment>
+    </CategoryProvider>
   );
 }
 
---
+++ src/context/CategoryContext.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+//Create context
+const CategoryContext = React.createContext();
+
+export const CategoryConsumer = CategoryContext.Consumer;
+
+class CategoryProvider extends Component {
+  token = "JJGPJBJUR32AB2ZPOALD";
+
+  state = {
+    categories: []
+  };
+
+  componentDidMount() {
+    this.getCategory();
+  }
+
+  getCategory = async () => {
+    // eslint-disable-next-line
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${
+      this.token
+    }&locale=es_ES`;
+    let categories = await axios.get(url);
+    this.setState({
+      categories: categories.data.categories
+    });
+  };
+
+  render() {
+    return (
+      <CategoryContext.Provider
+        value={{
+          categories: this.categories
+        }}
+      >
+        {this.props.children}
+      </CategoryContext.Provider>
+    );
+  }
+}
+
+export default CategoryProvider;
